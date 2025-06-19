872fe8ca4aacf858544f8bccd1137dba908bbefc
Clean18 <cleanage95@gmail.com>
2025-06-20 08:14:54 +0900

    Feat : 플레이어 방향키 방식 이동 기능
    이동 : 방향키
    공격 : Q

--- Assets/SJH/SJH_Scripts/PlayerControllerTypeB.cs
+++ Assets/SJH/SJH_Scripts/PlayerControllerTypeB.cs
@@ -1,18 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerTypeB : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float moveSpeed;
+	public float attackSpeed;
+	public Rigidbody2D rigid;
+	public Vector2 moveInput;
+	public GameObject effectPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Awake()
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		moveInput.x = Input.GetAxisRaw("Horizontal");
+		moveInput.y = Input.GetAxisRaw("Vertical");
+
+		if (Input.GetKeyDown(KeyCode.Q)) Attack();
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 movePos = moveInput.normalized * moveSpeed;
+		rigid.velocity = movePos;
+	}
+
+	void Attack()
+	{
+		Instantiate(effectPrefab, transform.position, transform.rotation).GetComponent<Rigidbody2D>().AddForce(moveInput * attackSpeed, ForceMode2D.Impulse);
+	}
 }
