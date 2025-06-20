9f0faf4071cdfa9ba00c461a38b69a31875b402e
Clean18 <cleanage95@gmail.com>
2025-06-21 00:38:26 +0900

    Delete : 플레이어 이동방식 결정으로 사용하지 않는 이동 삭제
    A로 결정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    // 
+    // 플레이어가 오토로 돌아갈때는 몬스터의 정보를 알아야함 > 몬스터를 추격하고 공격하기 위해
+    // 즉, 싱글톤이든 static이든 오브젝트풀이랑 몬스터들의 정보를 플레이어에서 접근할 수 있던가 해야함
 }
