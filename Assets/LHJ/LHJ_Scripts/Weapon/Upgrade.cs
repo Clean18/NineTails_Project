c909acc64694521e4a227ce82ab66034c8558414
neosmu <hojunglee00@naver.com>
2025-06-22 19:47:08 +0900

    [Feat] 강화 구현
    강화 테이블에 있는 내용들을 참조해서 강화 레벨에 따라 공격 스탯도 해당되는 데이터가 적용되게 구현(강화 버튼은 Test로 재화가 있다는 가정하에 Q를 누르면 다음 강화단계로 올라가게 구성)

--- Assets/LHJ/LHJ_Scripts/Weapon/Upgrade.cs
+++ Assets/LHJ/LHJ_Scripts/Weapon/Upgrade.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 /// <summary>
-/// 강화에 필요한 데이터
+/// 강화 정보 구조체
 /// </summary>
 [System.Serializable]
 public struct UpgradeInfo
@@ -26,6 +26,10 @@ public class UpgradeTable : DataTableParser<UpgradeInfo>
 public class Upgrade : MonoBehaviour
 {
     public UpgradeTable upgradeTable;
+    [SerializeField] private int currentGrade;      // 현재 장비 등급
+    [SerializeField] private int currentLevel;      // 현재 장비 강화
+    [SerializeField] private int currentAttack;     // 현재 장비 공격력 수치
+    [SerializeField] private int warmth;            // (Test용) 가지고 있는 재화
     void Start()
     {
         // 강화 데이터 다운로드 루틴 실행
@@ -51,5 +55,63 @@ public class Upgrade : MonoBehaviour
         };
         upgradeTable.Load(csv);
     }
-    // TODO: 강화로직 구현
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TryEnhance();
+        }
+    }
+    /// <summary>
+    /// 강화 테이블에서 현재 등급과 레벨에 맞는 다음 정보를 찾아 강화
+    /// 재화가 충분할 경우 강화 성공 처리 후 현재 공격력과 강화 레벨을 갱신
+    /// </summary>
+    public void TryEnhance()
+    {
+        // 강화 테이블에서 다음 강화 정보찾기
+        List<UpgradeInfo> upgradeList = upgradeTable.Values;
+        UpgradeInfo nextInfo = default;
+        bool found = false;
+
+        for (int i = 0; i < upgradeList.Count; i++)
+        {
+            UpgradeInfo info = upgradeList[i];
+
+            // 현재 등급과 다음 강화 단계 조건이 일치하는지
+            if (info.Grade == currentGrade && info.Level == currentLevel + 1)
+            {
+                nextInfo = info;
+                found = true;
+                break;
+            }
+        }
+
+        // 다음 강화 정보가 없을경우
+        if (!found)
+        {
+            Debug.Log("더 이상 강화할 수 없습니다.");
+            return;
+        }
+
+        // 재화 확인
+        if (warmth < nextInfo.WarmthCost)
+        {
+            Debug.Log("재화가");
+            return;
+        }
+        else
+        {
+            // 강화 성공 처리
+            warmth -= nextInfo.WarmthCost;
+            currentLevel += 1;
+            currentAttack = nextInfo.Attack;
+            Debug.Log("강화 성공! 현재 등급: " + currentGrade + " , 강화 단계: " + currentLevel + " , 공격력: " + currentAttack);
+        }
+
+        // 현재 강화 단계가 최대로 도달되었을때
+        if (currentLevel >= 5)
+        {
+            Debug.Log("최대 강화 도달");
+        }
+    }
 }
