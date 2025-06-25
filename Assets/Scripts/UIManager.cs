779ea09a47fe12dddc73d92329de063474688e53
neosmu <hojunglee00@naver.com>
2025-06-25 11:34:39 +0900

    [Refactor] 코드 디버그 내용 제거

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -25,7 +25,6 @@ public class UIManager : Singleton<UIManager>
 
             // 둘다 없을때 Resources에서 팝업캔버스 로드
             PopUpCanvas prefab = Resources.Load<PopUpCanvas>("PopUpCanvas");
-            Debug.Log($"PopUpCanvas 로드 결과: {prefab}");
             return Instantiate(prefab);
         }
     }
@@ -34,13 +33,6 @@ public class UIManager : Singleton<UIManager>
     public T ShowPopUp<T>() where T : BaseUI
     {
         T prefab = Resources.Load<T>($"PopUp/{typeof(T).Name}");
-        Debug.Log($"프리팹 로드 경로: PopUp/{typeof(T).Name}, 결과: {prefab}");
-
-        if (prefab == null)
-        {
-            Debug.LogError($"프리팹을 찾을 수 없습니다: Resources/PopUp/{typeof(T).Name}");
-            return null;
-        }
         T instance = Instantiate(prefab, PopUpCanvas.transform);
 
         PopUpCanvas.ShowUI(instance);
