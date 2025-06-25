2105b50e228a280fbfab2eedcd95244171dec583
YeonHouu <yeonhhou@gmail.com>
2025-06-25 23:04:32 +0900

    Refactor: 일반공격(1타) 리팩토링

--- Assets/CYH/CYH_Scripts/Skill/SkillLogic_0_0.cs
+++ Assets/CYH/CYH_Scripts/Skill/SkillLogic_0_0.cs
@@ -3,59 +3,65 @@ using UnityEngine;
 
 public class SkillLogic_0_0 : MonoBehaviour
 {
-    ActiveSkillData _data;
-    
-    [SerializeField] private Collider2D SwordCollider;
-    [SerializeField] Animator WeaponAnimator;
+    [SerializeField] private ActiveSkillData _data;
 
-    [SerializeField] private bool _isAttack = false;
+    private CapsuleCollider2D SwordCollider;
+    private Animator Animator;
 
-    private void Start()
+
+    private void Awake()
     {
-        //swordCollider.enabled = false;
+        SwordCollider = GetComponent<CapsuleCollider2D>();
+        Animator = GetComponent<Animator>();
+        
+        // 게임 스타트 -> 무기 collider 끔
+        SwordCollider.enabled = false;
     }
-  
+
     private void Update()
     {
-        AnimationPlay();
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             UseSkill();
+            AnimationPlay();
         }
+    }
 
-        AnimatorStateInfo stateInfo = WeaponAnimator.GetCurrentAnimatorStateInfo(0);
-        if (stateInfo.IsName("Skill_0") && stateInfo.normalizedTime >= 1.0f)
-        {
-            WeaponAnimator.SetBool("isAttack", false);
-        }
+    public void OnAttackStart()
+    {
+        SwordCollider.enabled = true;
+        Debug.Log("콜라이더 킴");
+    }
+
+    // 애니메이션이 끝났을 때 이벤트로 호출
+    public void OnAttackEnd()
+    {
+        SwordCollider.enabled = false;
+        Debug.Log("콜라이더 끔");
     }
 
     public void UseSkill()
     {
-        _isAttack = true;
+        OnAttackStart();
         Debug.Log("스킬사용");
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        Debug.Log("트리거");
-        //if (!swordCollider.enabled) return;
-        if (col.CompareTag("Monster"))
-        {
-            Debug.Log("공격");
-            //WeaponAnimator.SetBool("isAttack", false);
-        }
     }
 
     public void AnimationPlay()
     {
-        if (!_isAttack)
+        if (!SwordCollider.enabled)
             return;
         else
         {
-            SwordCollider.enabled = true;
-            WeaponAnimator.SetBool("isAttack", true);
-            _isAttack = false;
+            Animator.SetTrigger("UseSkill");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Monster"))
+        {
+            Debug.Log("몬스터 히트!");
+            // 데미지 로직 호출
         }
     }
 }
