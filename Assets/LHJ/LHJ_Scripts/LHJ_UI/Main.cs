c5a2d661b9fde56222a343be565b4e65cb680b06
neosmu <hojunglee00@naver.com>
2025-06-26 17:57:52 +0900

    [Feat] Main UI 구현(진행중)
    플레이어 스탯창 구현진행중 플레이어스탯 얼추 끝나면 추후 스킬창 구현 예정

--- Assets/LHJ/LHJ_Scripts/LHJ_UI/Main.cs
+++ Assets/LHJ/LHJ_Scripts/LHJ_UI/Main.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Main : BaseUI
 {
+    [SerializeField] private TextMeshProUGUI powerText;
+    [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI defenseText;
     private void Start()
     {
         GetEvent("Equipment").Click += data => UIManager.Instance.ShowPopUp<UpgradePopUp>();
         GetEvent("Setting").Click += data => UIManager.Instance.ShowPopUp<SettingPopUp>();
+        GetEvent("Stats").Click += data => UIManager.Instance.ShowPopUp<StatusPopUp>();
     }
 }
