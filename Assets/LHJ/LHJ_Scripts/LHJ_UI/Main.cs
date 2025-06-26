deca9e97cbc57c106c2a1e35ae6590731febb8bb
neosmu <hojunglee00@naver.com>
2025-06-26 20:37:36 +0900

    [Feat] 스탯창 구현
    스탯창 구현 수치는 코드 병합예정

--- Assets/LHJ/LHJ_Scripts/LHJ_UI/Main.cs
+++ Assets/LHJ/LHJ_Scripts/LHJ_UI/Main.cs
@@ -8,10 +8,21 @@ public class Main : BaseUI
     [SerializeField] private TextMeshProUGUI powerText;
     [SerializeField] private TextMeshProUGUI attackText;
     [SerializeField] private TextMeshProUGUI defenseText;
+    [SerializeField] private TextMeshProUGUI hpText;
     private void Start()
     {
         GetEvent("Equipment").Click += data => UIManager.Instance.ShowPopUp<UpgradePopUp>();
         GetEvent("Setting").Click += data => UIManager.Instance.ShowPopUp<SettingPopUp>();
         GetEvent("Stats").Click += data => UIManager.Instance.ShowPopUp<StatusPopUp>();
+        PlayerStatUI();
+    }
+
+    // 메인에 플레이어 스탯 정보UI
+    private void PlayerStatUI()
+    {
+        //powerText.text = $"Power Lev:{플레이어 최대체력}";
+        //attackText.text = $"Attack:{플레이어 공격력}";
+        //defenseText.text = $"Defense:{플레이어 방어력}";
+        //hpText.text = $"{체력}/{최대 체력}";
     }
 }
