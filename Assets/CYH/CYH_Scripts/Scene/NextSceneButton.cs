0516d00200740c2ceb56514300a67ceb448f2d29
YeonHouu <yeonhhou@gmail.com>
2025-07-04 23:27:17 +0900

    Feat: 버튼용 씬 전환 기능 추가

--- Assets/CYH/CYH_Scripts/Scene/NextSceneButton.cs
+++ Assets/CYH/CYH_Scripts/Scene/NextSceneButton.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NextSceneButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 버튼의 OnClick()에 연결
+    public void OnNextButtonClicked()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneChangeManager.Instance.LoadNextScene();
     }
 }
