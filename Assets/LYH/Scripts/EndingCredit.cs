fd4332810ff2715b0dad056511a67db4b4fc819b
YunhoLee <yl4643@nyu.edu>
2025-07-08 14:31:29 +0900

    Create : Ending Credit Scene

---
+++ Assets/LYH/Scripts/EndingCredit.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class EndingCredit : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer player;
+    [SerializeField] private GameObject GameEndUI;
+
+    private void Start()
+    {
+        player.loopPointReached += OnVideoEnd;
+    }
+
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        GameEndUI.SetActive(true);
+    }
+
+    public void previousScene()
+    {
+        Debug.Log("Go to 3-2 Battle Scene.");
+    }
+    public void EndGame()
+    {
+        Debug.Log("End Game.");
+    }
+}
